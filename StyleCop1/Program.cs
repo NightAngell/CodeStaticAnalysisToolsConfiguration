93166679aa83e4e412cb8a7a2030cb198a109983
Mateusz Soboń <sobta24@wp.pl>
2020-12-19 16:00:53 +0100

    Add project files.

---
+++ StyleCop1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace StyleCop1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.ReadKey();
+            Console.BackgroundColor = ConsoleColor.Red;
+            int hr = 2; // SA1121
+        }
+
+        private static void Test()
+        {
+        }
+    }
+}
