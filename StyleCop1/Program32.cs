1f4305bda1b197924d3a728bcf000006a8639541
Mateusz Soboń <sobta24@wp.pl>
2020-12-19 19:34:25 +0100

    configured StyleCop

---
+++ StyleCop1/Program32.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using static System.Console;
+
+namespace ConsoleApp
+{
+    internal class Program32
+    {
+        public class Test : IDisposable
+        {
+            public void Dispose()
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        internal static int Main(string[] args)
+        {
+            using (var xz = new Test())
+            using (var x = new Test())
+                return 3;
+        }
+    }
+}
