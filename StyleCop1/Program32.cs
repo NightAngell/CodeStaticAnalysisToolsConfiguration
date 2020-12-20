3a48f80394f3a97fc965419c633fb35a6a5556ba
Mateusz Soboń <sobta24@wp.pl>
2020-12-20 14:06:49 +0100

    Spacing Rules

--- StyleCop1/Program32.cs
+++ StyleCop1/Program32.cs
@@ -1,23 +1,65 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
 using static System.Console;
 
 namespace ConsoleApp
 {
+    /// <summary>
+    /// The main <c>Math</c> class.
+    /// Contains all methods for performing basic math functions.
+    /// <list type="bullet">
+    /// <item>
+    /// <term>Add</term>
+    /// <description>Addition Operation</description>
+    /// </item>
+    /// <item>
+    /// <term>Subtract</term>
+    /// <description>Subtraction Operation</description>
+    /// </item>
+    /// <item>
+    /// <term>Multiply</term>
+    /// <description>Multiplication Operation</description>
+    /// </item>
+    /// <item>
+    /// <term>Divide</term>
+    /// <description>Division Operation</description>
+    /// </item>
+    /// </list>
+    /// </summary>
     internal class Program32
     {
+        /// <value>Gets the value of PI.</value>
+        public static Program32 operator +(Program32 a, Program32 b) { return null; }
+
+        public int Xdd
+        {
+            get;
+
+            set;
+        }
+
+        /// <summary>
+        ///     Defines the entry point of the application.
+        /// 
+        /// </summary>
+        /// <param name="args">The arguments.</param>
+        internal static void Main(string[] args)
+        {
+            var a = new[] { 1, 10, 100, 1000 };
+            var aB = new[] { 1, 10, 100, 1000 };
+        }
+
+        public int MyProperty { get; set; }
+        public string Sss { get; set; }
+
         public class Test : IDisposable
         {
             public void Dispose()
             {
-                throw new NotImplementedException();
+                // throw new NotImplementedException();
             }
-        }
-
-        internal static int Main(string[] args)
-        {
-            using (var xz = new Test())
-            using (var x = new Test())
-                return 3;
         }
     }
 }
