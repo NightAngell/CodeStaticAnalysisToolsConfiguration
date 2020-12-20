935c196671ff9fa87a6d7c856aa72b1565db8b72
Mateusz Soboń <sobta24@wp.pl>
2020-12-20 18:46:50 +0100

    modifications

---
+++ StyleCop1/Genric{T,T2,TSuperUltraNinjaZKosmosu}.cs
@@ -0,0 +1,12 @@
+﻿// Copyright (c) Mateusz Soboń. All rights reserved.
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp
+{
+    public class Genric<T, T2, TSuperUltraNinjaZKosmosu>
+    {
+    }
+}
---
+++ StyleCop1/Program.cs
@@ -0,0 +1,20 @@
+﻿// Copyright (c) Mateusz Soboń. All rights reserved.
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using static System.Console;
+
+namespace ConsoleApp
+{
+    public class Program
+    {
+#pragma warning disable IDE0060 // Remove unused parameter
+        internal static void Main(string[] args)
+#pragma warning restore IDE0060 // Remove unused parameter
+        {
+        }
+
+    }
+}
