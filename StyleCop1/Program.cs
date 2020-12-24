e05510a0d92cd08dc2da3a510a848960e135ea6c
Mateusz Soboń <sobta24@wp.pl>
2020-12-24 10:43:16 +0100

    ordering rules

--- StyleCop1/Program.cs
+++ StyleCop1/Program.cs
@@ -4,12 +4,16 @@ using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
-using static System.Console;
+using ConsoleApp;
+using Lol = System;
 
 namespace ConsoleApp
 {
     public class Program
     {
+        public readonly int MyProperty;
+        public int MyProperty2;
+
 #pragma warning disable IDE0060 // Remove unused parameter
         internal static void Main(string[] args)
 #pragma warning restore IDE0060 // Remove unused parameter
@@ -17,7 +21,7 @@ namespace ConsoleApp
             var baseClass = new BaseClass("test");
             if (baseClass == null)
             {
-
+              
             }
         }
 
