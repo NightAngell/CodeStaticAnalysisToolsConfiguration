74fff369d4eb659bd64c7002f46ee2cb96e35041
Mateusz Soboń <sobta24@wp.pl>
2020-12-25 12:43:45 +0100

    Naming rules

--- StyleCop1/Program.cs
+++ StyleCop1/Program.cs
@@ -9,20 +9,16 @@ using Lol = System;
 
 namespace ConsoleApp
 {
+
     public class Program
     {
-        public readonly int MyProperty;
-        public int MyProperty2;
+        private int _xXDD = 5;
 
 #pragma warning disable IDE0060 // Remove unused parameter
         internal static void Main(string[] args)
 #pragma warning restore IDE0060 // Remove unused parameter
         {
-            var baseClass = new BaseClass("test");
-            if (baseClass == null)
-            {
-              
-            }
+            var m_number = 5;
         }
 
     }
