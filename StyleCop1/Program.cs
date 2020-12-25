7f72669b571bf2514d84f44d8a23dd06268ae9b7
Mateusz Soboń <sobta24@wp.pl>
2020-12-25 16:23:28 +0100

    Configured layout rules

--- StyleCop1/Program.cs
+++ StyleCop1/Program.cs
@@ -7,50 +7,14 @@ using Lol = System;
 
 namespace ConsoleApp
 {
-
-    /// <summary>
-    /// Violation of SA1506ElementDocumentationHeadersMustNotBeFollowedByBlankLine below
-    /// </summary>
-
     public class Program
     {
-
-#pragma warning disable IDE0060 // Remove unused parameter
-        internal static void Main(string[] args) // SA1505OpeningBracesMustNotBeFollowedByBlankLine
-#pragma warning restore IDE0060 // Remove unused parameter
+        internal static void Main()
         {
-            // Violation of SA1500BracesForMultiLineStatementsMustNotShareLine
-            if (true) {
-                int number = 5;
-                Console.WriteLine(number);
-            }
-
-            // Violation of SA1503BracesMustNotBeOmitted
-            if (true)
-                return;
         }
 
-
-
-        private bool _enabled = false;
-        public bool Enabled
+        public Program() 
         {
-            get { return _enabled; } // Violation of SA1504AllAccessorsMustBeSingleLineOrMultiLine
-
-            set
-            {
-                _enabled = value; // Violation of SA1508ClosingBracesMustNotBePrecededByBlankLine below
-
-            }
         }
-
-        // Violation of SA1501StatementMustNotBeOnSingleLine
-        public object Method()
-        {
-            lock (this) { return this; }
-        }
-
-        // Violation of SA1502ElementMustNotBeOnSingleLine
-        public object Method2() { return null; }
     }
 }
